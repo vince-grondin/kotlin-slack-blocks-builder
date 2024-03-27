f0839d2de6f4ebbba8db454332136c333407e3fb
vince-grondin <vince.grondin@gmail.com>
2024-03-26 20:40:45 -1000

    Add DSL marker for better scoping and annotate builders, add `ElementBuilder` parent builder for elements, add `ButtonBuilder`, define per_class test instance for all tests Refs #24

--- src/main/kotlin/eth/rochsolid/slack/blocks/dsl/compositionobjects/TextBuilder.kt
+++ src/main/kotlin/eth/rochsolid/slack/blocks/dsl/compositionobjects/TextBuilder.kt
@@ -1,17 +1,21 @@
 package eth.rochsolid.slack.blocks.dsl.compositionobjects
 
+import eth.rochsolid.slack.blocks.dsl.BlockDslMarker
 import eth.rochsolid.slack.blocks.models.compositionobjects.Text
 
+@BlockDslMarker
 sealed class TextBuilder<T : Text>(protected val text: String) {
     abstract fun build(): T
 }
 
+@BlockDslMarker
 class MarkdownBuilder(text: String) : TextBuilder<Text.Markdown>(text) {
     var verbatim: Boolean? = null
 
     override fun build(): Text.Markdown = Text.Markdown(text = text, verbatim = verbatim)
 }
 
+@BlockDslMarker
 class PlainTextBuilder(text: String) : TextBuilder<Text.PlainText>(text) {
     var emoji: Boolean? = null
 
